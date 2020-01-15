06b382a214d1031a104760c8466533d1a68196e5
Ilya Subochev <siveternal@gmail.com>
2020-01-15 15:59:22 +0300

    Movements

--- Game2048/Class1.cs
+++ Game2048/Class1.cs
@@ -6,7 +6,7 @@ namespace Game2048
     {
         Map map;
         bool isGameOver;
-
+        static Random random = new Random();
         public int size 
         { 
             get 
@@ -25,19 +25,68 @@ namespace Game2048
             for (int x = 0; x < size; x++)
                 for (int y = 0; y < size; y++)
                     map.Set(x, y, 0);
+            AddRandomNumber();
+            AddRandomNumber();
         }
 
+        private void AddRandomNumber()
+        {
+            if (isGameOver) return;
+            for (int j = 0; j < 100; j++)
+            {
+                int x = random.Next(0, map.size);
+                int y = random.Next(0, map.size);
+                if (map.Get(x, y) == 0)
+                {
+                    map.Set(x, y, random.Next(1, 3) * 2);
+                    return;
+                }
+            }
+        }
+
+        void Move(int x, int y, int sx, int sy)
+        {
+            if (map.Get(x, y) > 0)
+                while (map.Get(x + sx, y + sy) == 0)
+                {
+                    map.Set(x + sx, y + sy, map.Get(x, y));
+                    map.Set(x, y, 0);
+                    x += sx;
+                    y += sy;
+                }
+        }
         public void Left()
-        { }
+        {
+            for (int y = 0; y < map.size; y++)
+                for (int x = 1; x < map.size; x++)
+                    Move(x, y, -1, 0);
+            AddRandomNumber();
+        }
 
         public void Right()
-        { }
+        {
+            for (int y = 0; y < map.size; y++)
+                for (int x = map.size-2; x >=0; x--)
+                    Move(x, y, +1, 0);
+            AddRandomNumber();
+        }
 
         public void Up()
-        { }
+        {
+            for (int x = 0; x < map.size; x++)
+                for (int y = 1; y < map.size; y++)
+                    Move(x, y, 0, - 1);
+            AddRandomNumber();
+                
+        }
 
         public void Down()
-        { }
+        {
+            for (int x = 0; x < map.size; x++)
+                for (int y = map.size - 2; y >=0; y--)
+                    Move(x, y, 0, +1);
+            AddRandomNumber();
+        }
 
         public int GetMap(int x, int y)
         {
