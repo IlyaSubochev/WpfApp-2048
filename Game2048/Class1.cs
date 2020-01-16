a3c7db66fb82c618f6a989fa173cf2c6e73d17bb
Ilya Subochev <siveternal@gmail.com>
2020-01-16 16:17:59 +0300

    Add Join method

--- Game2048/Class1.cs
+++ Game2048/Class1.cs
@@ -55,27 +55,57 @@ namespace Game2048
                     y += sy;
                 }
         }
+
+        void Join(int x, int y, int sx, int sy)
+        {
+            if (map.Get(x, y) > 0)
+                if (map.Get(x + sx, y + sy) == map.Get(x, y))
+                {
+                    map.Set(x + sx, y + sy, map.Get(x, y) * 2);
+                    while (map.Get(x - sx, y - sy) > 0)
+                    {
+                        map.Set(x, y, map.Get(x - sx, y - sy));
+                        x -= sx;
+                        y -= sy;
+                    }
+                    map.Set(x, y, 0);
+                }
+
+        }
+
         public void Left()
         {
             for (int y = 0; y < map.size; y++)
+            {
                 for (int x = 1; x < map.size; x++)
                     Move(x, y, -1, 0);
+                for (int x = 1; x < map.size; x++)
+                    Join(x, y, -1, 0);
+            }
             AddRandomNumber();
         }
 
         public void Right()
         {
             for (int y = 0; y < map.size; y++)
-                for (int x = map.size-2; x >=0; x--)
+            {
+                for (int x = map.size - 2; x >= 0; x--)
                     Move(x, y, +1, 0);
+                for (int x = map.size - 2; x >= 0; x--)
+                    Join(x, y, +1, 0);
+            }
             AddRandomNumber();
         }
 
         public void Up()
         {
             for (int x = 0; x < map.size; x++)
+            {
                 for (int y = 1; y < map.size; y++)
-                    Move(x, y, 0, - 1);
+                    Move(x, y, 0, -1);
+                for (int y = 1; y < map.size; y++)
+                    Join(x, y, 0, -1);
+            }
             AddRandomNumber();
                 
         }
@@ -83,8 +113,12 @@ namespace Game2048
         public void Down()
         {
             for (int x = 0; x < map.size; x++)
-                for (int y = map.size - 2; y >=0; y--)
+            {
+                for (int y = map.size - 2; y >= 0; y--)
                     Move(x, y, 0, +1);
+                for (int y = map.size - 2; y >= 0; y--)
+                    Join(x, y, 0, +1);
+            }
             AddRandomNumber();
         }
 
